c1c4c011b373e6c86e60170cc1a1aee23bf2f970
Arbind Kumar <110377403+arbind8103@users.noreply.github.com>
2025-06-24 17:01:33 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ EMP_API_URL = "http://supremeautomobilerdc.com:81/personnel/api/employees/"
 AUTH = ('admin', 'xxxxxxxxx')
 
 #EMAIL_SENDER = "evergreenarbind@gmail.com"
-#EMAIL_PASSWORD = "ttcscfncrxxekwkq"
+#EMAIL_PASSWORD = "xxxxxxxxxxxx"
 #SMTP_SERVER = 'smtp.gmail.com'
 #SMTP_PORT = 465
